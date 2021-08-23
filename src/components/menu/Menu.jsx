34b924a2ae91c15615d28e4b3123f2af9a256a41
Clement <riera.clement@gmail.com>
2021-08-23 11:31:53 +0200

    video a mettre en place et arranger soulignement au hover du menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,21 +1,68 @@
-import "./menu.scss";
+import { useState } from 'react'
+import './menu.scss'
 
-const Menu = ({ menuOpen, setMenuOpen}) => {
+const Menu = ({ menuOpen, setMenuOpen }) => {
+  const [hover, setHover] = useState('before')
+  const [hover1, setHover1] = useState('before')
+  const [hover2, setHover2] = useState('before')
+
   return (
-    <div className={"menu " + (menuOpen && "active")}>
+    <div className={'menu ' + (menuOpen && 'active')}>
       <ul>
         <li onClick={() => setMenuOpen(false)}>
-          <a href="#intro">Acceuil</a>
+          <a
+            href="#intro"
+            onMouseOver={() => {
+              setHover('hover')
+            }}
+            onMouseLeave={() => {
+              setHover('after')
+
+              setTimeout(function () {
+                setHover('before')
+              }, 300)
+            }}
+          >
+            Acceuil <span id="span" className={hover}></span>
+          </a>
         </li>
         <li onClick={() => setMenuOpen(false)}>
-          <a href="#portefolio">Portefolio</a>
+          <a
+            href="#portefolio"
+            onMouseOver={() => {
+              setHover1('hover')
+            }}
+            onMouseLeave={() => {
+              setHover1('after')
+
+              setTimeout(function () {
+                setHover1('before')
+              }, 300)
+            }}
+          >
+            Portefolio <span id="span" className={hover1}></span>
+          </a>
         </li>
         <li onClick={() => setMenuOpen(false)}>
-          <a href="#contact">Contact</a>
+          <a
+            href="#contact"
+            onMouseOver={() => {
+              setHover2('hover')
+            }}
+            onMouseLeave={() => {
+              setHover2('after')
+
+              setTimeout(function () {
+                setHover2('before')
+              }, 300)
+            }}
+          >
+            Contact <span id="span" className={hover2}></span>
+          </a>
         </li>
       </ul>
     </div>
-  );
-};
+  )
+}
 
-export default Menu;
+export default Menu
--- src/components/portefolio/Portefolio.jsx
+++ src/components/portefolio/Portefolio.jsx
@@ -1,12 +1,58 @@
 import "./portefolio.scss";
 
+import ReactPlayer from 'react-player'
+import { useEffect } from "react";
+
 const Portefolio = () => {
+
+  useEffect(() => {
+    <ReactPlayer 
+        url="./assets/video2.mp4"
+        width="500px"
+        playing="true"
+        loop
+      />
+  }, [])
   return (
     <div className="portefolio" id="portefolio">
       <h1>Portefolio</h1>
-      <ul>
-        
-      </ul>
+      <div className="grid">
+      
+      <ReactPlayer 
+        url="./assets/video2.mp4"
+        width="500px"
+        playing
+        loop
+      />
+      
+{/*         <div className="projet">
+          <img src="./assets/photoClement.png" alt="" />
+          <div className="caption">
+            <video src="./assets/video.mp4"></video>
+          </div>
+          
+        </div>
+        <div className="projet">
+          <img src="./assets/photoClement.png" alt="" />
+          <div className="caption">
+            <video src="./assets/video.mp4"></video>
+          </div>
+          
+        </div>
+        <div className="projet">
+          <img src="./assets/photoClement.png" alt="" />
+          <div className="caption">
+          <video autoPlay width="500px" preload> 
+              <source src="./assets/video.mp4"
+              type="video/mp4" ></source>
+              c'est de la daube ton nav
+            </video>
+          </div>
+
+         
+          
+        </div> */}
+      </div>
     </div>
   );
 };
