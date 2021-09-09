00c8e6f84c8e40eb338606c5adaa76b220d67789
Clement <riera.clement@gmail.com>
2021-09-09 15:30:30 +0200

    page contact: integration faite

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,55 +5,56 @@ const Contact = () => {
   const [openForm, setOpenForm] = useState(false)
   return (
     <div className="contact" id="contact">
-      {!openForm &&<button
-        className="anim-btn"
-        onClick={() => {
-          setOpenForm(true)
-        }}
-      >
-        Parlez-moi!
-      </button>}
-      {!openForm &&  <h1>N'hesitez pas à me contacter</h1>}
+      {!openForm && (
+        <button
+          className="anim-btn"
+          onClick={() => {
+            setOpenForm(true)
+          }}
+        >
+          Parlez-moi!
+        </button>
+      )}
+      {!openForm && <h1>N'hesitez pas à me contacter</h1>}
       <div className="animation-form"></div>
-
 
       {openForm && (
         <div className="formulaire">
-          <h2>bonjour</h2>
-          <p>test</p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus error natus ut quas laboriosam repudiandae accusantium
-            assumenda labore tenetur, sint, alias maiores! Quam obcaecati
-            eligendi porro temporibus neque, accusantium qui voluptatum?
-            Molestias cumque soluta animi sit vitae quas, sed commodi neque ad
-            magnam unde officiis dicta deserunt, quam provident eligendi
-            blanditiis tempora ex in! Delectus pariatur, minima voluptatum sunt
-            recusandae ipsam, natus doloribus neque qui, accusamus sapiente.
-            Omnis, facere ea ipsum eligendi, hic totam obcaecati aspernatur
-            magni molestiae enim vitae natus a aliquam. Sint autem, nobis
-            expedita quae similique mollitia soluta, neque illo quos vel non
-            temporibus quo, facilis accusamus corporis. Totam at illum
-            voluptatibus labore perferendis est magnam pariatur temporibus
-            aperiam iure numquam illo in laborum deleniti, atque ipsum. Maiores
-            dolorem totam natus ad eligendi, sunt, officia optio exercitationem
-            iure laboriosam tempore. Commodi incidunt, modi ipsa ex odit soluta
-            aliquam animi itaque quis, tempora sint laboriosam aspernatur
-            deleniti deserunt! Ex, libero provident porro, blanditiis earum, aut
-            molestiae id ut exercitationem aliquid repudiandae atque odio sequi
-            enim! A, cum cumque. Aliquam deserunt magni consectetur sapiente
-            enim autem alias at optio, porro dolore inventore unde temporibus,
-            aperiam eligendi animi reprehenderit velit. Eos, at pariatur? Animi,
-            natus itaque. Consequatur laudantium quas vel!
-          </p>
-          <button
-            className="cancel-btn"
-            onClick={() => {
-              setOpenForm(false)
-            }}
-          >
-            Annuler
-          </button>
+          <div className="formulaire-titre">
+            <h2>Contact</h2>
+            <button
+              className="cancel-btn"
+              onClick={() => {
+                setOpenForm(false)
+              }}
+            >
+              <span>X</span> 
+            </button>
+            <p>
+              Laissez-moi un message et je vous repondrai le plus vite possible{' '}
+            </p>
+          </div>
+          <form action="">
+            <div className="container-firstline">
+              <label htmlFor="name" className="label-name">
+                Nom
+                <input type="text" htmlFor="name" />
+              </label>
+              <label htmlFor="entreprise" className="label-entreprise">
+                Entreprise
+                <input type="text" htmlFor="entreprise" />
+              </label>
+            </div>
+            <label htmlFor="email" className="label-email">
+              Email
+              <input type="email" htmlFor="email" />
+            </label>
+            <label htmlFor="message" className="label-message">
+              Message
+              <textarea htmlFor="message" rows="4"></textarea>
+            </label>
+            <button type="submit">Envoyer le message</button>
+          </form>
         </div>
       )}
     </div>
