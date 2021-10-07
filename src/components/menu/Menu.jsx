34b4032cf9380e68e3d6fe8e2a4b5bdf8d49c60a
Clement <riera.clement@gmail.com>
2021-10-07 19:12:14 +0200

    echec du carousel, a refaire depuis zero

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Mail, User } from 'react-feather'
 import './menu.scss'
 
 const Menu = ({ menuOpen, setMenuOpen }) => {
@@ -8,59 +9,71 @@ const Menu = ({ menuOpen, setMenuOpen }) => {
 
   return (
     <div className={'menu ' + (menuOpen && 'active')}>
-      <ul>
-        <li onClick={() => setMenuOpen(false)}>
-          <a
-            href="#intro"
-            onMouseOver={() => {
-              setHover('hover')
-            }}
-            onMouseLeave={() => {
-              setHover('after')
+      <div className="menu-wrapper">
+        <ul>
+          <li onClick={() => setMenuOpen(false)}>
+            <a
+              href="#intro"
+              onMouseOver={() => {
+                setHover('hover')
+              }}
+              onMouseLeave={() => {
+                setHover('after')
 
-              setTimeout(function () {
-                setHover('before')
-              }, 300)
-            }}
-          >
-            Acceuil <span id="span" className={hover}></span>
-          </a>
-        </li>
-        <li onClick={() => setMenuOpen(false)}>
-          <a
-            href="#portefolio"
-            onMouseOver={() => {
-              setHover1('hover')
-            }}
-            onMouseLeave={() => {
-              setHover1('after')
+                setTimeout(function () {
+                  setHover('before')
+                }, 300)
+              }}
+            >
+              Acceuil <span id="span" className={hover}></span>
+            </a>
+          </li>
+          <li onClick={() => setMenuOpen(false)}>
+            <a
+              href="#portefolio"
+              onMouseOver={() => {
+                setHover1('hover')
+              }}
+              onMouseLeave={() => {
+                setHover1('after')
 
-              setTimeout(function () {
-                setHover1('before')
-              }, 300)
-            }}
-          >
-            Portefolio <span id="span" className={hover1}></span>
-          </a>
-        </li>
-        <li onClick={() => setMenuOpen(false)}>
-          <a
-            href="#contact"
-            onMouseOver={() => {
-              setHover2('hover')
-            }}
-            onMouseLeave={() => {
-              setHover2('after')
+                setTimeout(function () {
+                  setHover1('before')
+                }, 300)
+              }}
+            >
+              Portefolio <span id="span" className={hover1}></span>
+            </a>
+          </li>
+          <li onClick={() => setMenuOpen(false)}>
+            <a
+              href="#contact"
+              onMouseOver={() => {
+                setHover2('hover')
+              }}
+              onMouseLeave={() => {
+                setHover2('after')
 
-              setTimeout(function () {
-                setHover2('before')
-              }, 300)
-            }}
-          >
-            Contact <span id="span" className={hover2}></span>
-          </a>
-        </li>
-      </ul>
+                setTimeout(function () {
+                  setHover2('before')
+                }, 300)
+              }}
+            >
+              Contact <span id="span" className={hover2}></span>
+            </a>
+          </li>
+        </ul>
+        <div className="info-contact">
+          <div className="itemContainer">
+            <User className="icon" />
+            <span>+336.37.22.28.76</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>riera.clement@gmail.com</span>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/portefolio/Portefolio.jsx
+++ src/components/portefolio/Portefolio.jsx
@@ -1,67 +1,38 @@
-import './portefolio.scss'
+import "./portefolio.scss";
 
-import Carousel from 'react-elastic-carousel'
-import { ArrowDown } from 'react-feather'
+import { ArrowDown, ArrowLeft, ArrowRight } from "react-feather";
+
+
 
 const Portefolio = () => {
-  const properties = {
-    itemsToShow: 2,
-    itemsToScroll: 1,
-  }
+
+  
 
   return (
     <div className="portefolio" id="portefolio">
-      <h1>Portefolio</h1>
+      <div className="first-block">
+        <h1>Portefolio</h1>
 
-      <ul className="categories">
-        <li> React</li>
-        <li>Node</li>
-        <li>JS</li>
-      </ul>
+        <ul className="categories">
+          <li>React</li>
+          <li>React</li>
+          <li>React</li>
 
-      <Carousel {...properties}>
-        <div className="projet">
-          <video autoPlay muted loop>
-            <source src="/assets/pepitas-react.mp4" type="video/mp4" />
-          </video>
-          <div className="caption">
-            <div className="langage front">
-              <ul>
-                <li>css</li>
-                <li>React</li>
-              </ul>
-            </div>
-            <div className="langage back">
-              <ul>
-                <li>Node</li>
-                <li>MongoDB</li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="projet">
-          <video autoPlay muted loop>
-            <source src="/assets/pepitas-react.mp4" type="video/mp4" />
-          </video>
-        </div>
-        <div className="projet">
-          <video autoPlay muted loop>
-            <source src="/assets/pepitas-react.mp4" type="video/mp4" />
-          </video>
-        </div>
-        <div className="projet">
-          <video autoPlay muted loop>
-            <source src="/assets/pepitas-react.mp4" type="video/mp4" />
-          </video>
-        </div>
-      </Carousel>
+          <li>JS</li>
+          <li>JS</li>
+          <li>JS</li>
+        </ul>
+      </div>
+     
+
+
       <div className="container-lien">
         <a href="#contact">
           <ArrowDown />
         </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Portefolio
+export default Portefolio;
